50944e964eae1c4ae5cfbfa83e0273e23d878127
chadcollins <chadrcollins91@gmail.com>
2018-11-19 19:34:04 -0500

    First test for creating instance

---
+++ packages/fn-l/src/__tests__/index.test.ts
@@ -0,0 +1,9 @@
+import {Fn} from "../index";
+import {expect} from 'chai';
+import 'jest';
+
+describe(`Interface tests`, () => {
+    it(`Can create Fn instance`, () => {
+        new Fn();
+    })
+});
