1a46a5dcfbabbdef9ea96685e54a1f8ebc71d4b9
chadcollins <chadrcollins91@gmail.com>
2018-11-19 22:06:08 -0500

    Implementation of short hand composition

--- packages/fn-l/src/__tests__/index.test.ts
+++ packages/fn-l/src/__tests__/index.test.ts
@@ -2,6 +2,14 @@ import * as Fn from "../index";
 import {expect} from 'chai';
 import 'jest';
 
+class MathFn {
+    add = (num: number) => (value: number) => value + num;
+    mul = (num: number) => (value: number) => value * num;
+
+    add3 = this.add(3);
+    mul2 = this.mul(2);
+}
+
 describe(`Interface tests`, () => {
     it(`Can create Fn instance`, () => {
         Fn.make();
@@ -14,18 +22,19 @@ describe(`Interface tests`, () => {
     });
 
     it(`Can compose two functions`, () => {
-        class MathFn2 {
-            add = (num: number) => (value: number) => value + num;
-            mul = (num: number) => (value: number) => value * num;
-
-            add3 = this.add(3);
-            mul2 = this.mul(2);
-        }
-
-        const fn = Fn.make(new MathFn2());
+        const fn = Fn.make(new MathFn());
 
         const add3_mul2 = fn.add3.mul2;
 
         expect(add3_mul2(4)).to.equal(14);
+    });
+
+    it(`Can use shorthand and to create function`, () => {
+        // Can only do shorthand notation with any type
+        const fn: any = Fn.make(new MathFn());
+
+        const add5 = fn.add$5;
+
+        expect(add5(2)).to.equal(7);
     });
 });
--- packages/fn-l/src/index.ts
+++ packages/fn-l/src/index.ts
@@ -4,35 +4,71 @@ type Fn<T> = {
     [P in keyof T]: Fn<T> & GenericFunction;
 }
 
+type ParamValue = string | number | boolean
+
+const optimisticCast = (value: string): ParamValue => {
+    const intVal = parseInt(value);
+    if (!isNaN(intVal)) {
+        return intVal;
+    }
+
+    if (value === "true") {
+        return true
+    } else if (value === "false") {
+        return false;
+    }
+
+    return value;
+};
+
 const makeFnProxyHandler = <T extends object> (): ProxyHandler<FnContextWrapper<T>> => {
     return {
         get: function (thisArg: FnContextWrapper<T>, prop: keyof T) {
             if (!!(thisArg.context.contextObject[prop])) {
-                return makeFnProxy(thisArg.context.contextObject, thisArg.context, prop);
+                return makeFnProxy(
+                    thisArg.context.contextObject,
+                    thisArg.context,
+                    prop
+                );
+            } else {
+                const strProp = prop.toString();
+                const indexOf$ = strProp.indexOf("$");
+                if (indexOf$ !== -1) {
+                    const methodName = strProp.slice(0, indexOf$);
+                    const value = strProp.slice(indexOf$ + 1);
+
+                    if (!!(thisArg.context.contextObject[methodName as keyof T])) {
+                        return makeFnProxy(
+                            thisArg.context.contextObject,
+                            thisArg.context,
+                            methodName as keyof T,
+                            optimisticCast(value)
+                        );
+                    }
+                }
             }
         },
         apply: function(target: FnContextWrapper<T>, thisArg: any, argumentsList: any) {
-            let methods = [];
+            let contexts = [];
 
-            // Grabbing here even though is same on all contexts in chain
-            let contextObject = target.context.contextObject;
-            let context = target.context;
+            // This is the last context in the chain
+            let lastContext = target.context;
 
-            // Get method chain
-            while (context.parent !== null) {
-                methods.push(context.key);
-                context = context.parent;
+            // Make array of contexts all the way up to parent
+            while (lastContext.parent !== null) {
+                contexts.push(lastContext);
+                lastContext = lastContext.parent;
             }
 
             // Reverse method chain for proper order
-            methods = methods.reverse();
+            contexts = contexts.reverse();
 
             // start with given argument
             let result = argumentsList[0];
 
             // Loop through methods passing the result of the previous to the next
-            for (const methodName of methods) {
-                let method: GenericFunction = contextObject[methodName] as unknown as GenericFunction;
+            for (const c of contexts) {
+                let method: GenericFunction = c.func;
                 result = method(result);
             }
 
@@ -45,15 +81,18 @@ class FnContext<T> {
     private readonly _contextObject: T;
     private readonly _parent?: FnContext<T>;
     private readonly _key?: keyof T;
+    private readonly _paramValue?: ParamValue;
 
     constructor(
         obj: T,
         parent: FnContext<T> = null,
-        key: keyof T = null
+        key: keyof T = null,
+        value: ParamValue = null
     ) {
         this._contextObject = obj;
         this._parent = parent;
         this._key = key;
+        this._paramValue = value;
     }
 
     get contextObject() {
@@ -67,6 +106,14 @@ class FnContext<T> {
     get key(): keyof T {
         return this._key;
     }
+
+    get func(): GenericFunction {
+        let f = this._contextObject[this._key] as unknown as GenericFunction;
+        if (this._paramValue) {
+            f = f(this._paramValue) as unknown as GenericFunction;
+        }
+        return f;
+    }
 }
 
 interface FnContextWrapper<T> {
@@ -76,16 +123,17 @@ interface FnContextWrapper<T> {
 const makeFnContext = <T> (
     obj: T,
     parent: FnContext<T> = null,
-    key: keyof T = null
+    key: keyof T = null,
+    value: ParamValue = null
 ) : FnContextWrapper<T> => {
     const func = () => {};
-    func.context = new FnContext<T>(obj, parent, key);
+    func.context = new FnContext<T>(obj, parent, key, value);
     return func;
 };
 
-const makeFnProxy = <T extends object> (obj: T, root: FnContext<T> = null, key: keyof T = null): Fn<T> => {
+const makeFnProxy = <T extends object> (obj: T, root: FnContext<T> = null, key: keyof T = null, value: ParamValue = null): Fn<T> => {
     return new Proxy(
-        makeFnContext(obj, root, key),
+        makeFnContext(obj, root, key, value),
         makeFnProxyHandler()
     ) as unknown as Fn<T>;
 };
