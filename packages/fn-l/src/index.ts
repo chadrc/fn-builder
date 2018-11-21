8c91e350e26cfaddd35860bf0ee5c06d22ed53a6
chadcollins <chadrcollins91@gmail.com>
2018-11-20 19:20:08 -0500

    Added early check to determine if using function composition to prevent calling entire chain every composition

--- packages/fn-l/src/index.ts
+++ packages/fn-l/src/index.ts
@@ -33,6 +33,26 @@ const makeFnProxyHandler = <T extends object>(): ProxyHandler<FnContextWrapper<T
             }
         },
         apply: function (target: FnContextWrapper<T>, thisArg: any, argumentsList: any) {
+            // Quick check to see if this context returns another function
+            try {
+                // Wrap in try-catch cause we don't now what function does
+                let r = target.context.func({});
+
+                // If this function returns a function
+                // consider this call a composition call
+                // instead of a call to retrieve this chains result
+                if (typeof r === "function") {
+                    target.context.paramValue = argumentsList[0];
+                    return new Proxy(
+                        target,
+                        makeFnProxyHandler()
+                    );
+                }
+            } catch (e) {
+                // Do nothing, since we don't know how the function works
+                // the parameter we passed may have caused the error
+            }
+
             let contexts = [];
 
             // This is the last context in the chain
