3d859fdeb3c4c9bcac2a877c02f9ade073708734
chadcollins <chadrcollins91@gmail.com>
2018-11-24 20:16:10 -0500

    (fn-builder-lodash) Simplified tests with sharing code

--- packages/fn-builder-lodash/src/__tests__/chunk.test.ts
+++ packages/fn-builder-lodash/src/__tests__/chunk.test.ts
@@ -3,29 +3,25 @@ import 'jest';
 import * as FnBuilder from "fn-builder";
 import "../chunk";
 import chunkFunction from "../functions/chunk";
+import {Fn} from "fn-builder/types";
+
+const testWithFn = (fn: Fn<any>) => () => {
+    const chunk = fn.chunk(2);
+
+    const input = ['a', 'b', 'c', 'd'];
+    const output = chunk(input);
+
+    expect(output).to.deep.equal([['a', 'b'], ['c', 'd']]);
+};
 
 describe(`chunk`, () => {
-    it(`can be included in custom Fn object`, () => {
-        const fn = FnBuilder.from({
+    it(`can be included in custom Fn object`, testWithFn(
+        FnBuilder.from({
             chunk: chunkFunction
-        });
+        })
+    ));
 
-        const chunk = fn.chunk(2);
-
-        const input = ['a', 'b', 'c', 'd'];
-        const output = chunk(input);
-
-        expect(output).to.deep.equal([['a', 'b'], ['c', 'd']]);
-    });
-
-    it(`can be imported dynamically`, () => {
-        const fn = FnBuilder.make();
-
-        const chunk = fn.chunk(2);
-
-        const input = ['a', 'b', 'c', 'd'];
-        const output = chunk(input);
-
-        expect(output).to.deep.equal([['a', 'b'], ['c', 'd']]);
-    });
+    it(`can be imported dynamically`, testWithFn(
+        FnBuilder.make()
+    ));
 });
--- packages/fn-builder-lodash/src/__tests__/compact.test.ts
+++ packages/fn-builder-lodash/src/__tests__/compact.test.ts
@@ -1,31 +1,27 @@
 import {expect} from 'chai';
 import 'jest';
 import * as FnBuilder from "fn-builder";
+import {Fn} from "fn-builder/types";
 import "../compact";
 import compactFunction from "../functions/compact";
 
+const testWithFn = (fn: Fn<any>) => () => {
+    const compact = fn.compact;
+
+    const input = [0, 1, false, 2, '', 3];
+    const output = compact(input);
+
+    expect(output).to.deep.equal([1, 2, 3]);
+};
+
 describe(`compact`, () => {
-    it(`can be included in custom Fn object`, () => {
-        const fn = FnBuilder.from({
+    it(`can be included in custom Fn object`, testWithFn(
+        FnBuilder.from({
             compact: compactFunction
-        });
+        })
+    ));
 
-        const compact = fn.compact;
-
-        const input = [0, 1, false, 2, '', 3];
-        const output = compact(input);
-
-        expect(output).to.deep.equal([1, 2, 3]);
-    });
-
-    it(`can be imported dynamically`, () => {
-        const fn = FnBuilder.make();
-
-        const compact = fn.compact;
-
-        const input = [0, 1, false, 2, '', 3];
-        const output = compact(input);
-
-        expect(output).to.deep.equal([1, 2, 3]);
-    });
+    it(`can be imported dynamically`, testWithFn(
+        FnBuilder.make()
+    ));
 });
