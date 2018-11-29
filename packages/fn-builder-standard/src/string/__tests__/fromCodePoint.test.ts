d5f494f9fa2a0add4ca3ef8e9e90abfd247e0f8c
chadcollins <chadrcollins91@gmail.com>
2018-11-28 19:09:20 -0500

    (fn-builder-standard) Fixed fromCodePoint test

--- packages/fn-builder-standard/src/string/__tests__/fromCodePoint.test.ts
+++ packages/fn-builder-standard/src/string/__tests__/fromCodePoint.test.ts
@@ -10,8 +10,9 @@ interface TestFn {
 
 const testWithFn = (fn: FnBuilder.FnBuilder<TestFn>) => () => {
     const func1 = fn.fromCodePoint.fn;
-    expect(func1(9731, 9733, 9842, 0x2F804)).to.equal("☃★♲你");
-    expect(func1([9731, 9733, 9842, 0x2F804])).to.equal("☃★♲你");
+    const expected = String.fromCodePoint(9731, 9733, 9842, 0x2F804);
+    expect(func1(9731, 9733, 9842, 0x2F804)).to.equal(expected);
+    expect(func1([9731, 9733, 9842, 0x2F804])).to.equal(expected);
 };
 
 describe("dropWhile", () => {
